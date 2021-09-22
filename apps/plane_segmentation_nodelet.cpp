9b240d338275996f3766a7ef35c5e21ed565e472
hriday <hriday.bavle@gmail.com>
2021-09-22 23:24:59 +0200

    Add subscriber for pointcloud in plane seg

--- apps/plane_segmentation_nodelet.cpp
+++ apps/plane_segmentation_nodelet.cpp
@@ -1,21 +1,46 @@
+#include <iostream>
+#include <string>
+#include <ros/time.h>
 #include <hdl_graph_slam/plane_segmentor.hpp>
 #include <nodelet/nodelet.h>
-
-#include <iostream>
-
+#include <pluginlib/class_list_macros.h>
+#include <pcl_ros/transforms.h>
+#include <pcl_ros/point_cloud.h>
+#include <tf/transform_listener.h>
 
 namespace hdl_graph_slam {
 
-class plane_segmentation_nodelet : public nodelet::Nodelet
-
+class PlaneSegmentationNodelet : public nodelet::Nodelet {
 public:
-plane_segmentation_nodelet() {}
-~plane_segmentation_nodelet() {}
+  typedef pcl::PointXYZRGB PointT;
 
+  PlaneSegmentationNodelet() {}
+  virtual ~PlaneSegmentationNodelet() {}
 
-/* publisher and subscribers */
-ros::Subscriber filtered_point_cloud_sub_;
-void filteredPointCloudCallback(const sensor_msgs::PointCloud2& msg);
+  virtual void onInit() {
+    nh = getNodeHandle();
+    private_nh = getPrivateNodeHandle();
+    this->init_ros();
+  }
 
-}
+private:
+  void init_ros() {
+    filtered_point_cloud_sub_ = nh.subscribe("filtered_point_cloud", 1, &PlaneSegmentationNodelet::filteredPointCloudCallback, this);
+  }
+  
+  void filteredPointCloudCallback(const pcl::PointCloud<PointT>::ConstPtr& msg) {
+    return;
+  }
 
+private:
+  ros::Subscriber filtered_point_cloud_sub_;
+
+  /* private variables */
+private:
+  ros::NodeHandle nh;
+  ros::NodeHandle private_nh;
+};
+
+}  // namespace hdl_graph_slam
+
+PLUGINLIB_EXPORT_CLASS(hdl_graph_slam::PlaneSegmentationNodelet, nodelet::Nodelet)
--- include/hdl_graph_slam/plane_segmentor.hpp
+++ include/hdl_graph_slam/plane_segmentor.hpp
@@ -9,7 +9,13 @@
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
-
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/approximate_voxel_grid.h>
+#include <pcl/filters/radius_outlier_removal.h>
+#include <pcl/filters/statistical_outlier_removal.h>
 
 namespace hdl_graph_slam {
 
