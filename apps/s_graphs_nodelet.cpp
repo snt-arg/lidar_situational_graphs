8c278f3bfd832385bfdbb660e6f79d9d8bd3036c
hriday <hriday.bavle@gmail.com>
2022-07-22 11:52:45 +0200

    Add neighbour mapper

--- apps/s_graphs_nodelet.cpp
+++ apps/s_graphs_nodelet.cpp
@@ -70,6 +70,7 @@
 #include <s_graphs/plane_utils.hpp>
 #include <s_graphs/room_mapper.hpp>
 #include <s_graphs/plane_mapper.hpp>
+#include <s_graphs/neighbour_mapper.hpp>
 
 #include <g2o/vertex_room.hpp>
 #include <g2o/vertex_corridor.hpp>
@@ -126,6 +127,7 @@ public:
     plane_mapper.reset(new PlaneMapper(private_nh));
     inf_room_mapper.reset(new InfiniteRoomMapper(private_nh));
     finite_room_mapper.reset(new FiniteRoomMapper(private_nh));
+    neighbour_mapper.reset(new NeighbourMapper(private_nh));
 
     gps_time_offset = private_nh.param<double>("gps_time_offset", 0.0);
     gps_edge_stddev_xy = private_nh.param<double>("gps_edge_stddev_xy", 10000.0);
@@ -296,8 +298,8 @@ private:
           inf_room_mapper->lookup_corridors(graph_slam, PlaneUtils::plane_class::Y_VERT_PLANE, room_data, x_vert_planes, y_vert_planes, dupl_x_vert_planes, dupl_y_vert_planes, x_corridors, y_corridors, rooms_vec);
         }
       }
-      // TODO:HB factor neighbours appropriately in the sgraphs
-      // factor_room_neighbours(room_data_msg);
+
+      // neighbour_mapper->factor_room_neighbours(graph_slam, room_data_msg, x_corridors, y_corridors, rooms_vec);
       room_data_queue.pop_front();
     }
   }
@@ -309,295 +311,6 @@ private:
   void detect_room_neighbours(const s_graphs::RoomsData& room_msg) {
     for(const auto& room_data : room_msg.rooms) {
     }
-  }
-
-  /**
-   * @brief factor the room neighbours between two rooms/corridors
-   *
-   */
-  void factor_room_neighbours(const s_graphs::RoomsData& room_msg) {
-    // here we have all the room detected connected ids filled it along with its neighbours
-    for(const auto& room_data : room_msg.rooms) {
-      // get the room node in the graph with detected room connected id
-      auto found_current_room = std::find_if(rooms_vec.begin(), rooms_vec.end(), boost::bind(&Rooms::connected_id, _1) == room_data.id);
-      if(found_current_room != rooms_vec.end()) {
-        // loop over the connected_ids of the found room
-        for(const auto& connected_neighbour_id : (*found_current_room).connected_neighbour_ids) {
-          auto found_detected_neighbour = std::find_if(room_msg.rooms.begin(), room_msg.rooms.end(), boost::bind(&s_graphs::RoomData::id, _1) == connected_neighbour_id);
-          // find each connected neighbours node in the graph
-          auto found_current_room_neighbour = std::find_if(rooms_vec.begin(), rooms_vec.end(), boost::bind(&Rooms::connected_id, _1) == connected_neighbour_id);
-          // if the found neighbour is a room, add room/room edge
-          if(found_current_room_neighbour != rooms_vec.end()) {
-            Eigen::Vector2d room_room_meas = room_room_measurement(room_data, (*found_detected_neighbour));
-            factor_room_room_constraints((*found_current_room), (*found_current_room_neighbour), room_room_meas);
-          } else {
-            auto found_current_x_corridor_neighbour = std::find_if(x_corridors.begin(), x_corridors.end(), boost::bind(&Corridors::connected_id, _1) == connected_neighbour_id);
-            // if the found neighbour is x_corridor add room/x_corridor edge
-            if(found_current_x_corridor_neighbour != x_corridors.end()) {
-              double room_x_corr_meas = room_x_corridor_measurement(room_data, (*found_detected_neighbour));
-              factor_room_x_corridor_constraints((*found_current_room), (*found_current_x_corridor_neighbour), room_x_corr_meas);
-            } else {
-              auto found_current_y_corridor_neighbour = std::find_if(y_corridors.begin(), y_corridors.end(), boost::bind(&Corridors::connected_id, _1) == connected_neighbour_id);
-              // if the found neighbour is y_corridor add room/y_corridor edge
-              if(found_current_y_corridor_neighbour != y_corridors.end()) {
-                double room_y_corr_meas = room_y_corridor_measurement(room_data, (*found_detected_neighbour));
-                factor_room_y_corridor_constraints((*found_current_room), (*found_current_y_corridor_neighbour), room_y_corr_meas);
-              }
-            }
-          }
-        }
-        (*found_current_room).connected_id = -1;
-        (*found_current_room).connected_neighbour_ids.clear();
-      } else {
-        auto found_current_x_corridor = std::find_if(x_corridors.begin(), x_corridors.end(), boost::bind(&Corridors::connected_id, _1) == room_data.id);
-        if(found_current_x_corridor != x_corridors.end()) {
-          // loop over the current x_corridor neighbours
-          for(const auto& connected_neighbour_id : (*found_current_x_corridor).connected_neighbour_ids) {
-            auto found_detected_neighbour = std::find_if(room_msg.rooms.begin(), room_msg.rooms.end(), boost::bind(&s_graphs::RoomData::id, _1) == connected_neighbour_id);
-            // find each connected neighbours node in the graph
-            auto found_current_room_neighbour = std::find_if(rooms_vec.begin(), rooms_vec.end(), boost::bind(&Rooms::connected_id, _1) == connected_neighbour_id);
-            if(found_current_room_neighbour != rooms_vec.end()) {
-              double x_corr_room_meas = room_x_corridor_measurement((*found_detected_neighbour), room_data);
-              factor_x_corridor_room_constraints((*found_current_x_corridor), (*found_current_room_neighbour), x_corr_room_meas);
-            } else {
-              auto found_current_x_corridor_neighbour = std::find_if(x_corridors.begin(), x_corridors.end(), boost::bind(&Corridors::connected_id, _1) == connected_neighbour_id);
-              if(found_current_x_corridor_neighbour != x_corridors.end()) {
-                double x_corr_x_corr_meas = x_corridor_x_corridor_measurement(room_data, (*found_detected_neighbour));
-                factor_x_corridor_x_corridor_constraints((*found_current_x_corridor), (*found_current_x_corridor_neighbour), x_corr_x_corr_meas);
-              } else {
-                auto found_current_y_corridor_neighbour = std::find_if(y_corridors.begin(), y_corridors.end(), boost::bind(&Corridors::connected_id, _1) == connected_neighbour_id);
-                if(found_current_y_corridor_neighbour != y_corridors.end()) {
-                  // not adding edge here
-                  factor_x_corridor_y_corridor_constraints((*found_current_x_corridor), (*found_current_y_corridor_neighbour));
-                }
-              }
-            }
-          }
-          (*found_current_x_corridor).connected_id = -1;
-          (*found_current_x_corridor).connected_neighbour_ids.clear();
-        } else {
-          auto found_current_y_corridor = std::find_if(y_corridors.begin(), y_corridors.end(), boost::bind(&Corridors::connected_id, _1) == room_data.id);
-          if(found_current_y_corridor != y_corridors.end()) {
-            // loop over the current y_corridor neighbours
-            for(const auto& connected_neighbour_id : (*found_current_y_corridor).connected_neighbour_ids) {
-              auto found_detected_neighbour = std::find_if(room_msg.rooms.begin(), room_msg.rooms.end(), boost::bind(&s_graphs::RoomData::id, _1) == connected_neighbour_id);
-              // find each connected neighbours node in the graph
-              auto found_current_room_neighbour = std::find_if(rooms_vec.begin(), rooms_vec.end(), boost::bind(&Rooms::connected_id, _1) == connected_neighbour_id);
-              if(found_current_room_neighbour != rooms_vec.end()) {
-                double y_corr_room_meas = room_y_corridor_measurement(room_data, (*found_detected_neighbour));
-                factor_y_corridor_room_constraints((*found_current_y_corridor), (*found_current_room_neighbour), y_corr_room_meas);
-              } else {
-                auto found_current_x_corridor_neighbour = std::find_if(x_corridors.begin(), x_corridors.end(), boost::bind(&Corridors::connected_id, _1) == connected_neighbour_id);
-                if(found_current_x_corridor_neighbour != x_corridors.end()) {
-                  // not adding any edge here
-                  factor_y_corridor_x_corridor_constraints((*found_current_y_corridor), (*found_current_x_corridor_neighbour));
-                } else {
-                  auto found_current_y_corridor_neighbour = std::find_if(y_corridors.begin(), y_corridors.end(), boost::bind(&Corridors::connected_id, _1) == connected_neighbour_id);
-                  if(found_current_y_corridor_neighbour != y_corridors.end()) {
-                    double y_corr_y_corr_meas = y_corridor_y_corridor_measurement(room_data, (*found_detected_neighbour));
-                    factor_y_corridor_y_corridor_constraints((*found_current_y_corridor), (*found_current_y_corridor_neighbour), y_corr_y_corr_meas);
-                  }
-                }
-              }
-            }
-            (*found_current_y_corridor).connected_id = -1;
-            (*found_current_y_corridor).connected_neighbour_ids.clear();
-          }
-        }
-      }
-    }
-  }
-
-  /**
-   * @brief get the current pose between the two rooms
-   *
-   */
-  Eigen::Vector2d room_room_measurement(const s_graphs::RoomData& room_msg_1, const s_graphs::RoomData& room_msg_2) {
-    Eigen::Vector2d room_room_meas(0, 0);
-    room_room_meas(0) = pow(room_msg_1.room_center.x - room_msg_2.room_center.x, 2);
-    room_room_meas(1) = pow(room_msg_1.room_center.y - room_msg_2.room_center.y, 2);
-
-    return room_room_meas;
-  }
-
-  /**
-   * @brief get the current pose between the room and x_corridor
-   *
-   */
-  double room_x_corridor_measurement(const s_graphs::RoomData& room_msg, const s_graphs::RoomData& x_corridor_msg) {
-    double room_x_corr_meas;
-    room_x_corr_meas = pow(room_msg.room_center.x - x_corridor_msg.room_center.x, 2);
-
-    return room_x_corr_meas;
-  }
-
-  /**
-   * @brief get the current pose between the room and y_corridor
-   *
-   */
-  double room_y_corridor_measurement(const s_graphs::RoomData& room_msg, const s_graphs::RoomData& y_corridor_msg) {
-    double room_y_corr_meas;
-    room_y_corr_meas = pow(room_msg.room_center.y - y_corridor_msg.room_center.y, 2);
-
-    return room_y_corr_meas;
-  }
-
-  double x_corridor_x_corridor_measurement(const s_graphs::RoomData& x_corridor_msg1, const s_graphs::RoomData& x_corridor_msg2) {
-    double x_corr_x_corr_meas;
-    x_corr_x_corr_meas = pow(x_corridor_msg1.room_center.x - x_corridor_msg2.room_center.x, 2);
-
-    return x_corr_x_corr_meas;
-  }
-
-  double y_corridor_y_corridor_measurement(const s_graphs::RoomData& y_corridor_msg1, const s_graphs::RoomData& y_corridor_msg2) {
-    double y_corr_y_corr_meas;
-    y_corr_y_corr_meas = pow(y_corridor_msg1.room_center.y - y_corridor_msg2.room_center.y, 2);
-
-    return y_corr_y_corr_meas;
-  }
-
-  /**
-   * @brief factor edges between neighbouring rooms
-   *
-   */
-  void factor_room_room_constraints(s_graphs::Rooms& room1, const s_graphs::Rooms& room2, Eigen::Vector2d room_room_meas) {
-    bool neighbour_exists = false;
-    for(const auto& room1_neighbour_id : room1.neighbour_ids) {
-      if(room2.id == room1_neighbour_id) {
-        neighbour_exists = true;
-        break;
-      }
-    }
-    if(!neighbour_exists) room1.neighbour_ids.push_back(room2.id);
-
-    // TODO:HB add an edge in the graph between room1 and room2
-    Eigen::Matrix2d information;
-    information.setIdentity();
-    auto edge = graph_slam->add_room_room_edge(room1.node, room2.node, room_room_meas, information);
-    graph_slam->add_robust_kernel(edge, "Huber", 1.0);
-  }
-
-  /**
-   * @brief factor edges between room x_corridor
-   *
-   */
-  void factor_room_x_corridor_constraints(s_graphs::Rooms& room, const s_graphs::Corridors& x_corridor, double room_x_corr_meas) {
-    bool neighbour_exists = false;
-    for(const auto& room_neighbour_id : room.neighbour_ids) {
-      if(x_corridor.id == room_neighbour_id) {
-        neighbour_exists = true;
-        break;
-      }
-    }
-    if(!neighbour_exists) room.neighbour_ids.push_back(x_corridor.id);
-
-    // TODO:HB add an edge in the graph between room1 and x_corridor
-    Eigen::Matrix<double, 1, 1> information(1);
-    auto edge = graph_slam->add_room_x_corridor_edge(room.node, x_corridor.node, room_x_corr_meas, information);
-    graph_slam->add_robust_kernel(edge, "Huber", 1.0);
-  }
-
-  /**
-   * @brief factor edges between room andy corridor
-   *
-   */
-  void factor_room_y_corridor_constraints(s_graphs::Rooms& room, const s_graphs::Corridors& y_corridor, double room_y_corr_meas) {
-    bool neighbour_exists = false;
-    for(const auto& room_neighbour_id : room.neighbour_ids) {
-      if(y_corridor.id == room_neighbour_id) {
-        neighbour_exists = true;
-        break;
-      }
-    }
-    if(!neighbour_exists) room.neighbour_ids.push_back(y_corridor.id);
-
-    Eigen::Matrix<double, 1, 1> information(1);
-    auto edge = graph_slam->add_room_y_corridor_edge(room.node, y_corridor.node, room_y_corr_meas, information);
-    graph_slam->add_robust_kernel(edge, "Huber", 1.0);
-  }
-
-  void factor_x_corridor_room_constraints(s_graphs::Corridors& x_corridor, const s_graphs::Rooms& room, double x_corr_room_meas) {
-    bool neighbour_exists = false;
-    for(const auto& x_corridor_neighbour_id : x_corridor.neighbour_ids) {
-      if(room.id == x_corridor_neighbour_id) {
-        neighbour_exists = true;
-        break;
-      }
-    }
-    if(!neighbour_exists) x_corridor.neighbour_ids.push_back(room.id);
-
-    Eigen::Matrix<double, 1, 1> information(1);
-    auto edge = graph_slam->add_room_x_corridor_edge(room.node, x_corridor.node, x_corr_room_meas, information);
-    graph_slam->add_robust_kernel(edge, "Huber", 1.0);
-  }
-
-  void factor_x_corridor_x_corridor_constraints(s_graphs::Corridors& x_corridor1, const s_graphs::Corridors& x_corridor2, double x_corr_x_corr_meas) {
-    bool neighbour_exists = false;
-    for(const auto& x_corridor_neighbour_id : x_corridor1.neighbour_ids) {
-      if(x_corridor2.id == x_corridor_neighbour_id) {
-        neighbour_exists = true;
-        break;
-      }
-    }
-
-    if(!neighbour_exists) x_corridor1.neighbour_ids.push_back(x_corridor2.id);
-    Eigen::Matrix<double, 1, 1> information(1);
-    auto edge = graph_slam->add_x_corridor_x_corridor_edge(x_corridor1.node, x_corridor2.node, x_corr_x_corr_meas, information);
-    graph_slam->add_robust_kernel(edge, "Huber", 1.0);
-  }
-
-  void factor_x_corridor_y_corridor_constraints(s_graphs::Corridors& x_corridor, const s_graphs::Corridors& y_corridor) {
-    bool neighbour_exists = false;
-    for(const auto& x_corridor_neighbour_id : x_corridor.neighbour_ids) {
-      if(y_corridor.id == x_corridor_neighbour_id) {
-        neighbour_exists = true;
-        break;
-      }
-    }
-    if(!neighbour_exists) x_corridor.neighbour_ids.push_back(y_corridor.id);
-    // TODO:HB analyze if edge between x and y will be useful??
-  }
-
-  void factor_y_corridor_room_constraints(s_graphs::Corridors& y_corridor, const s_graphs::Rooms& room, double y_corr_room_meas) {
-    bool neighbour_exists = false;
-    for(const auto& y_corridor_neighbour_id : y_corridor.neighbour_ids) {
-      if(room.id == y_corridor_neighbour_id) {
-        neighbour_exists = true;
-        break;
-      }
-    }
-    if(!neighbour_exists) y_corridor.neighbour_ids.push_back(room.id);
-
-    Eigen::Matrix<double, 1, 1> information(1);
-    auto edge = graph_slam->add_room_y_corridor_edge(room.node, y_corridor.node, y_corr_room_meas, information);
-    graph_slam->add_robust_kernel(edge, "Huber", 1.0);
-  }
-
-  void factor_y_corridor_x_corridor_constraints(s_graphs::Corridors& y_corridor, const s_graphs::Corridors& x_corridor) {
-    bool neighbour_exists = false;
-    for(const auto& y_corridor_neighbour_id : y_corridor.neighbour_ids) {
-      if(x_corridor.id == y_corridor_neighbour_id) {
-        neighbour_exists = true;
-        break;
-      }
-    }
-    if(!neighbour_exists) y_corridor.neighbour_ids.push_back(x_corridor.id);
-    // TODO:HB analyze if edge between x and y will be useful??
-  }
-
-  void factor_y_corridor_y_corridor_constraints(s_graphs::Corridors& y_corridor1, const s_graphs::Corridors& y_corridor2, double y_corr_y_corr_meas) {
-    bool neighbour_exists = false;
-    for(const auto& y_corridor1_neighbour_id : y_corridor1.neighbour_ids) {
-      if(y_corridor2.id == y_corridor1_neighbour_id) {
-        neighbour_exists = true;
-        break;
-      }
-    }
-    if(!neighbour_exists) y_corridor1.neighbour_ids.push_back(y_corridor2.id);
-
-    Eigen::Matrix<double, 1, 1> information(1);
-    auto edge = graph_slam->add_y_corridor_y_corridor_edge(y_corridor1.node, y_corridor2.node, y_corr_y_corr_meas, information);
-    graph_slam->add_robust_kernel(edge, "Huber", 1.0);
   }
 
   /**
@@ -2817,9 +2530,10 @@ private:
   std::unique_ptr<NmeaSentenceParser> nmea_parser;
   std::unique_ptr<InformationMatrixCalculator> inf_calclator;
   std::unique_ptr<PlaneUtils> plane_utils;
+  std::unique_ptr<PlaneMapper> plane_mapper;
   std::unique_ptr<InfiniteRoomMapper> inf_room_mapper;
   std::unique_ptr<FiniteRoomMapper> finite_room_mapper;
-  std::unique_ptr<PlaneMapper> plane_mapper;
+  std::unique_ptr<NeighbourMapper> neighbour_mapper;
 };
 
 }  // namespace s_graphs
