6d929eb00a69a5a42a9e6b1d8b7e70b8dd54a07e
Pedro <pmbs.123@gmail.com>
2022-09-28 21:09:23 +0200

    Bug fix - Added the arg -r to the rosdep install command + added line to install catkin-tools

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# - Installing catkin tools
+sudo pip3 install -U catkin_tools
+
+# - Installing vcstool
+sudo apt install python3-vcstool
+
 # - Importing all dependencies
 vcs import --recursive ../ < .rosinstall
-cd ../../ && rosdep install --from-paths src -ignore-src -y
+cd ../../ && rosdep install --from-paths src --ignore-src -r -y
 catkin build
 
 # - Sourcing the workspace
