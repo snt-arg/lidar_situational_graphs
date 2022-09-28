3ed5252b92c327e7fa96a8b1f4649ca1123e5b77
PedroS235 <pmbs.12@hotmail.com>
2022-09-28 14:40:52 +0200

    Update - Removed the unusefull comments

--- setup.sh
+++ setup.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Assuming that we don't have a sperate repo for the s_graphs workspace, the script will only install the dependencies alongside the s_graph core.
-# This can later be modified to make it the same as the stugalux_ws
-
+# - Importing all dependencies
 vcs import --recursive ../ < .rosinstall
 cd ../../ && rosdep install --from-paths src -ignore-src -y
 catkin build
 
 # - Sourcing the workspace
 source devel/setup.bash
-
-
-# - Here we could also download the datasets if they are online
