b51d9b2fa1e9fa92262e181a227dc1035fc0fe5c
shaheer34mts <muhammad_shaheer_ahmed@outlook.com>
2022-11-16 11:32:36 +0100

    Add constructor and destructor

--- src/s_graphs/wall_mapper.cpp
+++ src/s_graphs/wall_mapper.cpp
@@ -1,3 +1,10 @@
 // SPDX-License-Identifier: BSD-2-Clause
 
 #include <s_graphs/wall_mapper.hpp>
+
+namespace s_graphs {
+
+WallMapper::WallMapper(const ros::NodeHandle& private_nh) {}
+WallMapper::~WallMapper() {}
+
+}  // namespace s_graphs
