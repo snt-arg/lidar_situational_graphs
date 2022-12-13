3447f3ec02c9580cad41b746c93dc23ac64db222
shaheer34mts <muhammad_shaheer_ahmed@outlook.com>
2022-12-13 13:23:17 +0100

    [feat] - add graph_publisher class

---
+++ include/s_graphs/graph_publisher.hpp
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: BSD-2-Clause
+
+#ifndef GRAPH_PUBLISHER_HPP
+#define GRAPH_PUBLISHER_HPP
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <math.h>
+#include <boost/format.hpp>
+
+#include <ros/ros.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <geometry_msgs/Point.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/conversions.h>
+
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/common/common.h>
+#include <pcl_ros/transforms.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+#include <s_graphs/RoomData.h>
+#include <s_graphs/RoomsData.h>
+#include <s_graphs/PlaneData.h>
+#include <s_graphs/PlanesData.h>
+#include <s_graphs/graph_slam.hpp>
+#include <s_graphs/planes.hpp>
+#include <s_graphs/infinite_rooms.hpp>
+#include <s_graphs/rooms.hpp>
+#include <s_graphs/plane_utils.hpp>
+#include <s_graphs/graph_slam.hpp>
+#include <s_graphs/keyframe.hpp>
+
+#include <g2o/vertex_room.hpp>
+#include <g2o/vertex_wall.hpp>
+#include <g2o/vertex_infinite_room.hpp>
+#include <g2o/types/slam3d/edge_se3.h>
+#include <g2o/types/slam3d/vertex_se3.h>
+#include <g2o/edge_se3_plane.hpp>
+#include <g2o/edge_se3_priorxy.hpp>
+#include <g2o/edge_se3_priorxyz.hpp>
+#include <g2o/edge_se3_priorvec.hpp>
+#include <g2o/edge_se3_priorquat.hpp>
+#include <g2o/types/slam3d_addons/vertex_plane.h>
+#include <g2o/edge_se3_point_to_plane.hpp>
+
+#include <g2o/edge_infinite_room_plane.hpp>
+#include <g2o/edge_room.hpp>
+#include <g2o/edge_wall_two_planes.hpp>
+#include <unordered_map>
+
+class GraphPublisher {
+public:
+  GraphPublisher(const ros::NodeHandle& private_nh);
+  ~GraphPublisher();
+
+public:
+  void convert_graph_to_string(std::unique_ptr<s_graphs::GraphSLAM>& graph_slam);
+
+private:
+};
+
+#endif  // GRAPH_PUBLISHER_HPP
---
+++ src/s_graphs/graph_publisher.cpp
@@ -0,0 +1,34 @@
+#include <graph_publisher.hpp>
+
+GraphPublisher::GraphPublisher(const ros::NodeHandle& private_nh) {}
+
+GraphPublisher::~GraphPublisher() {}
+void GraphPublisher::convert_graph_to_string(std::unique_ptr<s_graphs::GraphSLAM>& graph_slam) {
+  g2o::SparseOptimizer* local_graph = graph_slam->graph.get();
+  auto edge_itr = local_graph->edges().begin();
+  for(int i = 0; edge_itr != local_graph->edges().end(); edge_itr++, i++) {
+    g2o::HyperGraph::Edge* edge = *edge_itr;
+    g2o::EdgeRoom4Planes* edge_r4p = dynamic_cast<g2o::EdgeRoom4Planes*>(edge);
+    if(edge_r4p) {
+      g2o::VertexRoomXYLB* v_room;
+      g2o::VertexPlane* v_xplane1;
+      g2o::VertexPlane* v_xplane2;
+      g2o::VertexPlane* v_yplane1;
+      g2o::VertexPlane* v_yplane2;
+      std::cout << "room edge found !  :  " << i << '\n';
+    }
+  }
+  //   auto vertex_itr = local_graph->vertices().begin();
+  //   for(int i = 0; vertex_itr != local_graph->edges().end(); vertex_itr++, i++) {
+  //     g2o::HyperGraph::Edge* edge = *vertex_itr;
+  //     g2o::EdgeSE3* edge_se3 = dynamic_cast<g2o::EdgeSE3*>(edge);
+  //     if(edge_se3) {
+  //       g2o::VertexSE3* v1 = dynamic_cast<g2o::VertexSE3*>(edge_se3->vertices()[0]);
+  //       g2o::VertexSE3* v2 = dynamic_cast<g2o::VertexSE3*>(edge_se3->vertices()[1]);
+  //       Eigen::Vector3d pt1 = v1->estimate().translation();
+  //       Eigen::Vector3d pt2 = v2->estimate().translation();
+  //     }
+  //   }
+
+  //   g2o::VertexSE3*
+}
