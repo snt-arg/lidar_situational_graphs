cd0bc77b8065c62b2c5e0ec2798302791a79fc8b
shaheer34mts <muhammad_shaheer_ahmed@outlook.com>
2022-12-14 16:17:53 +0100

    [feat] - add room nodes in graph publisher

--- src/s_graphs/graph_publisher.cpp
+++ src/s_graphs/graph_publisher.cpp
@@ -8,55 +8,110 @@ ros1_graph_manager_interface::Graph GraphPublisher::publish_graph(std::unique_pt
   std::vector<ros1_graph_manager_interface::Edge> edges_vec;
   std::vector<ros1_graph_manager_interface::Node> nodes_vec;
   ros1_graph_manager_interface::Graph graph_msg;
-  std::vector<ros1_graph_manager_interface::Attribute> att_vec;
+  std::vector<ros1_graph_manager_interface::Attribute> edge_att_vec;
+  std::vector<ros1_graph_manager_interface::Attribute> node_att_vec;
+
+  // Graph Type
+  if(graph_type == "BIM") {
+    graph_msg.name = "BIM";
+  } else {
+    graph_msg.name = "ONLINE";
+  }
+
+  // iterate over graph
   auto edge_itr = local_graph->edges().begin();
   for(int i = 0; edge_itr != local_graph->edges().end(); edge_itr++, i++) {
     g2o::HyperGraph::Edge* edge = *edge_itr;
     g2o::EdgeRoom4Planes* edge_r4p = dynamic_cast<g2o::EdgeRoom4Planes*>(edge);
+    g2o::EdgeRoom2Planes* edge_r2p = dynamic_cast<g2o::EdgeRoom2Planes*>(edge);
+    // g2o::EdgePlaneParallel* edge_pp = dynamic_cast<g2o::EdgePlaneParallel*>(edge);
     if(edge_r4p) {
       g2o::VertexRoomXYLB* v_room = dynamic_cast<g2o::VertexRoomXYLB*>(edge_r4p->vertices()[0]);
       g2o::VertexPlane* v_xplane1 = dynamic_cast<g2o::VertexPlane*>(edge_r4p->vertices()[1]);
       g2o::VertexPlane* v_xplane2 = dynamic_cast<g2o::VertexPlane*>(edge_r4p->vertices()[2]);
       g2o::VertexPlane* v_yplane1 = dynamic_cast<g2o::VertexPlane*>(edge_r4p->vertices()[3]);
       g2o::VertexPlane* v_yplane2 = dynamic_cast<g2o::VertexPlane*>(edge_r4p->vertices()[4]);
-      ros1_graph_manager_interface::Edge edge_nodes;
-      edge_nodes.origin_node = v_room->id();
-      edge_nodes.target_node = v_xplane1->id();
-      ros1_graph_manager_interface::Attribute edge_attributes;
-      edge_attributes.name = "EdgeRoom4Planes";
-      att_vec.push_back(edge_attributes);
-      edge_nodes.attributes = att_vec;
-      edges_vec.push_back(edge_nodes);
-      att_vec.clear();
-      edge_nodes.target_node = v_xplane2->id();
-      att_vec.push_back(edge_attributes);
-      edge_nodes.attributes = att_vec;
-      edges_vec.push_back(edge_nodes);
-      att_vec.clear();
-      edge_nodes.target_node = v_yplane1->id();
-      att_vec.push_back(edge_attributes);
-      edge_nodes.attributes = att_vec;
-      edges_vec.push_back(edge_nodes);
-      att_vec.clear();
-      edge_nodes.target_node = v_yplane2->id();
-      att_vec.push_back(edge_attributes);
-      edge_nodes.attributes = att_vec;
-      edges_vec.push_back(edge_nodes);
-      att_vec.clear();
+      ros1_graph_manager_interface::Edge graph_edge;
+      ros1_graph_manager_interface::Node graph_node;
+      ros1_graph_manager_interface::Attribute edge_attribute;
+      ros1_graph_manager_interface::Attribute node_attribute;
+
+      // Room Node
+      graph_node.id = v_room->id();
+      graph_node.type = "Room";
+      node_attribute.name = "VertexRoomXYLB";
+      Eigen::Vector2d room_pose = v_room->estimate();
+      node_attribute.fl_value.push_back(room_pose.x());
+      node_attribute.fl_value.push_back(room_pose.y());
+      node_att_vec.push_back(node_attribute);
+      graph_node.attributes = node_att_vec;
+      nodes_vec.push_back(graph_node);
+      node_attribute.fl_value.clear();
+      node_att_vec.clear();
+
+      // first edge
+      graph_edge.origin_node = v_room->id();
+      graph_edge.target_node = v_xplane1->id();
+      edge_attribute.name = "EdgeRoom4Planes";
+      edge_att_vec.push_back(edge_attribute);
+      graph_edge.attributes = edge_att_vec;
+      edges_vec.push_back(graph_edge);
+      edge_att_vec.clear();
+
+      // 2nd edge
+      graph_edge.target_node = v_xplane2->id();
+      edge_att_vec.push_back(edge_attribute);
+      graph_edge.attributes = edge_att_vec;
+      edges_vec.push_back(graph_edge);
+      edge_att_vec.clear();
+
+      // 3rd edge
+      graph_edge.target_node = v_yplane1->id();
+      edge_att_vec.push_back(edge_attribute);
+      graph_edge.attributes = edge_att_vec;
+      edges_vec.push_back(graph_edge);
+      edge_att_vec.clear();
+
+      // 4th edge
+      graph_edge.target_node = v_yplane2->id();
+      edge_att_vec.push_back(edge_attribute);
+      graph_edge.attributes = edge_att_vec;
+      edges_vec.push_back(graph_edge);
+      edge_att_vec.clear();
       // std::cout << "v_room : " << v_room->id() << std::endl;
       // std::cout << "v_xplane1 : " << v_xplane1->id() << '\n';
       // std::cout << "v_xplane2 : " << v_xplane2->id() << '\n';
       // std::cout << "v_yplane1 : " << v_yplane1->id() << '\n';
       // std::cout << "v_yplane2 : " << v_yplane2->id() << '\n';
-    } else {
-      continue;
+    } else if(edge_r2p) {
+      g2o::VertexRoomXYLB* v_room = dynamic_cast<g2o::VertexRoomXYLB*>(edge_r2p->vertices()[0]);
+      g2o::VertexPlane* v_xplane = dynamic_cast<g2o::VertexPlane*>(edge_r2p->vertices()[1]);
+      g2o::VertexPlane* v_yplane = dynamic_cast<g2o::VertexPlane*>(edge_r2p->vertices()[2]);
+      ros1_graph_manager_interface::Edge graph_edge;
+      graph_edge.origin_node = v_room->id();
+      graph_edge.target_node = v_xplane->id();
+      ros1_graph_manager_interface::Attribute edge_attribute;
+      edge_attribute.name = "EdgeRoom2Planes";
+      edge_att_vec.push_back(edge_attribute);
+      graph_edge.attributes = edge_att_vec;
+      edges_vec.push_back(graph_edge);
+      edge_att_vec.clear();
+      graph_edge.origin_node = v_room->id();
+      graph_edge.target_node = v_yplane->id();
+      edge_attribute.name = "EdgeRoom2Planes";
+      edge_att_vec.push_back(edge_attribute);
+      graph_edge.attributes = edge_att_vec;
+      edges_vec.push_back(graph_edge);
+      edge_att_vec.clear();
+      // } else if(edge_pp) {
+      //   g2o::VertexPlane* v_plane1 = dynamic_cast<g2o::VertexPlane*>(edge_r4p->vertices()[0]);
+      //   g2o::VertexPlane* v_plane2 = dynamic_cast<g2o::VertexPlane*>(edge_r4p->vertices()[1]);
+      // }
     }
-
     // std::cout << " edges_vec size : " << edges_vec.size() << std::endl;
   }
   graph_msg.edges = edges_vec;
-  if(graph_type == "BIM") {
-    graph_msg.name = "BIM";
-  }
+
+  edges_vec.clear();
   return graph_msg;
 }
