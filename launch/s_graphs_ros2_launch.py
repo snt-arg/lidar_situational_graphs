6895e4b577069744913be88bcaf74db23a4d5c34
hriday <hriday.bavle@gmail.com>
2023-01-25 15:18:34 +0100

    [Feat] add args in launch file

--- launch/s_graphs_ros2_launch.py
+++ launch/s_graphs_ros2_launch.py
@@ -1,62 +1,88 @@
-# Copyright 2021 Intelligent Robotics Lab
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 import os
-
-from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from ament_index_python import get_package_share_directory
+from launch.actions import OpaqueFunction
+from launch.conditions import IfCondition, UnlessCondition
 
 
 def generate_launch_description():
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "compute_odom",
+                default_value="true",
+                description="Flag to compute the odometry",
+            ),
+            DeclareLaunchArgument(
+                "env",
+                default_value="real",
+                description="Flag to get the environment type real or sim",
+            ),
+            OpaqueFunction(function=launch_sgraphs),
+        ]
+    )
 
-    pkg_dir = get_package_share_directory('s_graphs')
-    prefilitering_param_file = os.path.join(
-        pkg_dir, 'config', 'prefiltering.yaml')
 
-    prefiltering_cmd = Node(package='s_graphs', executable='s_graphs_prefiltering_node', parameters=[prefilitering_param_file], output='screen', remappings=[('velodyne_points', '/platform/velodyne_points'),
-                                                                                                                                                             ('imu/data', '/platform/imu/data')]
-                            )
+def launch_sgraphs(context, *args, **kwargs):
+    pkg_dir = get_package_share_directory("s_graphs")
+    prefiltering_param_file = os.path.join(pkg_dir, "config", "prefiltering.yaml")
+    scan_matching_param_file = os.path.join(pkg_dir, "config", "scan_matching.yaml")
+    s_graphs_param_file = os.path.join(pkg_dir, "config", "s_graphs.yaml")
 
-    pkg_dir = get_package_share_directory('s_graphs')
-    scan_matching_param_file = os.path.join(
-        pkg_dir, 'config', 'scan_matching.yaml')
+    prefiltering_cmd = Node(
+        package="s_graphs",
+        executable="s_graphs_prefiltering_node",
+        parameters=[prefiltering_param_file],
+        output="screen",
+        remappings=[
+            ("velodyne_points", "/platform/velodyne_points"),
+            ("imu/data", "/platform/imu/data"),
+        ],
+    )
 
-    scan_matching_cmd = Node(package='s_graphs', executable='s_graphs_scan_matching_odometry_node', parameters=[scan_matching_param_file], output='screen'
-                             )
+    compute_odom_arg = LaunchConfiguration("compute_odom").perform(context)
 
-    pkg_dir = get_package_share_directory('s_graphs')
-    room_segmentation_cmd = Node(package='s_graphs', executable='s_graphs_room_segmentation_node', parameters=[{"vertex_neigh_thres": 2}], output='screen'
-                                 )
+    scan_matching_cmd = Node(
+        package="s_graphs",
+        executable="s_graphs_scan_matching_odometry_node",
+        parameters=[scan_matching_param_file],
+        remappings=[("/odom", "/platform/odometry")],
+        output="screen",
+        condition=IfCondition(compute_odom_arg),
+    )
 
-    pkg_dir = get_package_share_directory('s_graphs')
-    floor_plan_cmd = Node(package='s_graphs', executable='s_graphs_floor_plan_node', parameters=[{"vertex_neigh_thres": 2}], output='screen'
-                          )
+    room_segmentation_cmd = Node(
+        package="s_graphs",
+        executable="s_graphs_room_segmentation_node",
+        parameters=[{"vertex_neigh_thres": 2}],
+        output="screen",
+    )
 
-    s_graphs_param_file = os.path.join(
-        pkg_dir, 'config', 's_graphs.yaml')
-    pkg_dir = get_package_share_directory('s_graphs')
-    s_graphs_cmd = Node(package='s_graphs', executable='s_graphs_node', parameters=[s_graphs_param_file], output='screen', remappings=[]
-                        )
+    floor_plan_cmd = Node(
+        package="s_graphs",
+        executable="s_graphs_floor_plan_node",
+        parameters=[{"vertex_neigh_thres": 2}],
+        output="screen",
+    )
 
-    ld = LaunchDescription()
-    ld.add_action(prefiltering_cmd)
-    ld.add_action(scan_matching_cmd)
-    ld.add_action(room_segmentation_cmd)
-    ld.add_action(floor_plan_cmd)
-    ld.add_action(s_graphs_cmd)
+    s_graphs_cmd = Node(
+        package="s_graphs",
+        executable="s_graphs_node",
+        parameters=[s_graphs_param_file],
+        output="screen",
+        remappings=[
+            ("velodyne_points", "/platform/velodyne_points"),
+            ("/odom", "/platform/odometry"),
+        ],
+    )
 
-    return ld
+    return [
+        prefiltering_cmd,
+        scan_matching_cmd,
+        room_segmentation_cmd,
+        floor_plan_cmd,
+        s_graphs_cmd,
+    ]
