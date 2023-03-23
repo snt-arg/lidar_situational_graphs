2681b029a4dd336a0da8f6ce160b176827096c85
Miguel <miferco97@gmail.com>
2023-03-23 17:57:24 +0100

    [fix] remove PCD storage

--- include/s_graphs/room_utils.hpp
+++ include/s_graphs/room_utils.hpp
@@ -1,3 +1,32 @@
+/*
+Copyright (c) 2023, University of Luxembourg
+All rights reserved.
+
+Redistributions and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+*/
+
 #ifndef __ROOM_UTILS_HPP_
 #define __ROOM_UTILS_HPP_
 #include <g2o/types/slam3d/edge_se3.h>
@@ -262,19 +291,7 @@ class RoomsKeyframeGenerator : public s_graphs::Rooms {
       }
     }
     // std::cout << "max_dist:" << max_dist << std::endl;
-
-    /************/
     room_keyframe_dict_.insert({ext_room.id, ext_room});
-
-    auto filtered = filter_room_pointcloud<s_graphs::PointT>(cloud, max_dist);
-    std::string preable(
-        "/home/miguel/lux_stay_ws/ros2_ws/src/multirobot_sgraphs_server/"
-        "pointcloud_data/room_keyframes/");
-    pcl::io::savePCDFileASCII(
-        preable + "room_keyframe_" + std::to_string(room.id) + ".pcd", *cloud);
-    pcl::io::savePCDFileASCII(
-        preable + "room_keyframe_" + std::to_string(room.id) + "filtered.pcd",
-        *filtered);
   }
 
   const ExtendedRooms& getExtendedRoom(int id) { return room_keyframe_dict_[id]; };
