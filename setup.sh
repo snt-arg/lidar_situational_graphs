a05db5d7e993026029ddb39c3ead34f5e3daabf6
Hriday Bavle <hriday.bavle@gmail.com>
2024-04-15 11:39:20 +0200

    [Feat] Add reasoning_ws inside s_graphs_utils

--- setup.sh
+++ setup.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-# - Installing catkin tools
-sudo pip3 install -U catkin_tools
-
 # - Installing vcstool
 sudo apt install python3-vcstool
 
+#install python dependencies
+pip3 install torch==2.0.1+cu118 -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install torch-geometric==2.3.1
+pip3 install torchvision==0.15.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
+pip3 install torch-sparse==0.6.17+pt20cu118 -f https://data.pyg.org/whl/torch-2.0.1+cu118.html
+pip3 install pyg-lib -f https://data.pyg.org/whl/torch-2.0.1+cu118.html
+pip install protobuf==3.20.*
+
+#rosdep init
+rosdep init && rosdep update --include-eol-distros
+
+#import all repos
+vcs import --recursive ../ < .rosinstall_ros2
+
+#install python requirements
+pip3 install -r requirements.txt
+
+#rosdep install
+cd ../../ && rosdep install --from-paths src -y --ignore-src -r
+
 # - Importing all dependencies
-vcs import --recursive ../ < .rosinstall
-cd ../../ && rosdep install --from-paths src --ignore-src -r -y
-catkin build
+colcon build
 
 # - Sourcing the workspace
-source devel/setup.bash
+source install/setup.bash
