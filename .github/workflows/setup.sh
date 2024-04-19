15899e06483c06c178b54be1850c0f404c764b07
Hriday Bavle <hriday.bavle@gmail.com>
2024-04-19 14:31:50 +0200

    [Fix] Fix github workflows

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# - Installing cpp dependencies
 sudo apt update
+sudo apt install -y libceres-dev
+
+#rosdep init
+rosdep update --include-eol-distros
 
 #import all repos
-vcs import --recursive < .rosinstall_ros2
+git submodule update --init --recursive
+
+#install python requirements
+pip3 install -r requirements.txt
 
 #rosdep install
 rosdep install --from-paths . -y --ignore-src -r
+
+# - Importing all dependencies
+colcon build --symlink-install
