de845e9fcabbe196e66c8df50a570e974b618e3c
Hriday Bavle <hriday.bavle@gmail.com>
2024-04-23 08:06:57 +0200

    [Fix] Update workflows

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 sudo apt update
-DEBIAN_FRONTEND=noninteractive sudo apt install -y libceres-dev
+DEBIAN_FRONTEND=noninteractive sudo apt install -y libceres-dev python3-pip
 
 #rosdep init
 rosdep update --include-eol-distros
 
 #import all repos
 git submodule update --init --recursive
-
-#install python requirements
-pip3 install -r requirements.txt
 
 #rosdep install
 DEBIAN_FRONTEND=noninteractive rosdep install --from-paths . -y --ignore-src -r
