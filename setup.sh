d242f5717922c70aea49f52af0dea99bd998cba6
Hriday Bavle <hriday.bavle@gmail.com>
2024-06-14 17:02:33 +0200

    [Feat] Remove torch vision from debs

--- setup.sh
+++ setup.sh
@@ -9,7 +9,6 @@ sudo apt install -y python3-pip
 #install python dependencies
 pip3 install torch==2.0.1+cu118 -f https://download.pytorch.org/whl/torch_stable.html
 pip3 install torch-geometric==2.3.1
-pip3 install torchvision==0.15.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
 pip3 install torch-sparse==0.6.17+pt20cu118 -f https://data.pyg.org/whl/torch-2.0.1+cu118.html
 pip3 install pyg-lib -f https://data.pyg.org/whl/torch-2.0.1+cu118.html
 pip install protobuf==3.20.*
